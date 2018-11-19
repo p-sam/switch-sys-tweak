2ec659c31dadd67ff18e7766f8d40d1648249de3
p-sam <p-sam@d3vs.net>
2018-11-19 22:22:31 +0000

    🌛  Initial commit

---
+++ source/nsvm_mitm_service.hpp
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2018 Atmosphère-NX
+ * Copyright (c) 2018 p-sam
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+#pragma once
+#include <switch.h>
+#include "libstratosphere.hpp"
+
+enum NsVmCmd : u32 {
+	NsVmCmd_NeedsUpdateVulnerability = 1200,
+};
+
+class NsVmMitmService : public IMitmServiceObject {
+	public:
+		NsVmMitmService(std::shared_ptr<Service> s) : IMitmServiceObject(s) {
+			/* ... */
+		}
+
+		static bool ShouldMitm(u64 pid, u64 tid) {
+			/* Only MitM LibraryAppletWeb. */
+			return tid == 0x010000000000100AULL;
+		}
+
+		static void PostProcess(IMitmServiceObject *obj, IpcResponseContext *ctx);
+
+	protected:
+		/* Overridden commands. */
+		Result NeedsUpdateVulnerability(OutPointerWithServerSize<u8, 0x1> out);
+	public:
+		DEFINE_SERVICE_DISPATCH_TABLE {
+			MakeServiceCommandMeta<NsVmCmd_NeedsUpdateVulnerability, &NsVmMitmService::NeedsUpdateVulnerability>(),
+		};
+};
